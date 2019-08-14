3e5916c53ec645ac9a2aa8bf8633c77f82d06580
Praveen Anna Haridas <aha.praveen@gmail.com>
2019-08-14 12:29:43 -0400

    Initial commit

--- stepdefs/step_demo.js
+++ stepdefs/step_demo.js
@@ -18,18 +18,14 @@ var EC = protractor.ExpectedConditions;
 
 
 Given('I go on {string}', function (url) {
-    // Write code here that turns the phrase above into concrete actions
-    browser.waitForAngularEnabled(false);
     browser.driver.get(url);
 });
 
 Then('the title should equal {string}', function (text) {
-    // Write code here that turns the phrase above into concrete actions
     return expect(browser.getTitle()).to.eventually.equal(text); 
 });
 
 When('I add todo task {string}',{timeout: 60*1000}, function (text){
-    browser.waitForAngularEnabled(true);
     element(by.model('todoList.todoText')).sendKeys(text);
     element(by.css('[value="add"]')).click();   
 });
